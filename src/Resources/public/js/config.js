d0a90cce47c7dbce2a1d4d72ababd01b35a50846
Alex Rothberg <agrothberg@gmail.com>
2024-11-22 15:40:57 -0500

    Check pimcore.settings['data-hub-writeable']

--- src/Resources/public/js/config.js
+++ src/Resources/public/js/config.js
@@ -209,7 +209,7 @@ pimcore.plugin.datahub.config = Class.create({
         menu.add(new Ext.menu.Item({
             text: t('clone'),
             iconCls: "pimcore_icon_clone",
-            disabled: !this.userIsAllowedToCreate(record.data.adapter),
+            disabled: !pimcore.settings['data-hub-writeable'] || !this.userIsAllowedToCreate(record.data.adapter),
             handler: this.cloneConfiguration.bind(this, tree, record)
         }));
 
